18c851f9ac43c68c55dcb3ffd39c8bad058c681a
Seong-dong <darkquiz@naver.com>
2023-01-20 00:35:49 +0900

    cd

--- prod-web-autoscale/main.tf
+++ prod-web-autoscale/main.tf
@@ -2,7 +2,8 @@
 provider "aws" {
   region                  = "ap-northeast-2"
   profile                 = "22shop"
-  shared_credentials_file = "C:/Users/aa/.aws/credentials"
+  # shared_credentials_file = "C:/Users/aa/.aws/credentials"
+  shared_credentials_file = "~/.aws/credentials"
   #4.x버전의 AWS공급자 허용
   version = "~> 4.0"
 
